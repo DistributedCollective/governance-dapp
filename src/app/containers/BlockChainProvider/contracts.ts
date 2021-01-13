fe73ab8b206d0342f3a932ef50243e3978ae0453
pietro-maximoff <pietro.maxim.official@protonmail.com>
2021-01-13 13:22:26 +0300

    update  governance UI

--- src/app/containers/BlockChainProvider/contracts.ts
+++ src/app/containers/BlockChainProvider/contracts.ts
@@ -7,7 +7,7 @@ import ERC20TokenABI from './abi/ERC20Token.json';
 export const contracts: IContractNetworks = {
   testnet: {
     sovToken: {
-      address: '0x04fa98E97A376a086e3BcAB99c076CB249e5740D',
+      address: '0x4c2bE6C604A3181Ef531914671902c2De068EA63',
       abi: ERC20TokenABI as any,
     },
     staking: {
@@ -15,11 +15,11 @@ export const contracts: IContractNetworks = {
       abi: StakingABI as any,
     },
     timelock: {
-      address: '0x462Ac951756EE704e5116F247071D2663A41fF9d',
+      address: '0x0E9fb5B82bD46320d811104542EEE4209536978a',
       abi: TimelockABI as any,
     },
     governorAlpha: {
-      address: '0xD4f5b2F1Ce0b743018F0fa71F37208f4550B48E8',
+      address: '0x2ed3014C631F249BEf8f2930dD9833AECe0798DC',
       abi: GovernorAlphaABI as any,
     },
   },
