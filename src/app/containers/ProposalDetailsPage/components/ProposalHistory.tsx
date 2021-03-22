7fa134a28fbc8fd2a7773d299aebcb18c42e55c2
pietro-maximoff <pietro.maxim.official@protonmail.com>
2021-03-22 23:02:37 +0300

    fix issue #20

--- src/app/containers/ProposalDetailsPage/components/ProposalHistory.tsx
+++ src/app/containers/ProposalDetailsPage/components/ProposalHistory.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Icon } from '@blueprintjs/core';
 import { EventData } from 'web3-eth-contract';
 import { Proposal } from 'types/Proposal';
+import { Popover2 } from '@blueprintjs/popover2';
+import { translations } from 'locales/i18n';
+import { toastSuccess } from 'utils/toaster';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { dateByBlocks, prettyTx } from 'utils/helpers';
 
 interface Props {
@@ -9,6 +15,7 @@ interface Props {
 }
 
 export function ProposalHistory(props: Props) {
+  const { t } = useTranslation();
   return (
     <>
       {!props.proposal ? (
@@ -25,14 +32,41 @@ export function ProposalHistory(props: Props) {
                 Proposed by:
               </p>
               <div className="w-auto">
-                <p className="text-gold text-sm tracking-normal">
-                  {props.proposal?.proposer && (
-                    <>
+                {props.proposal?.proposer && (
+                  <Popover2
+                    minimal={true}
+                    placement="top"
+                    popoverClassName="bp3-tooltip2"
+                    content={
+                      <div className="flex items-center">
+                        <p className="text-gold text-sm tracking-normal">
+                          {props.proposal?.proposer || '0x00000000000000000'}
+                        </p>
+                        <CopyToClipboard
+                          onCopy={() =>
+                            toastSuccess(
+                              <>{t(translations.onCopy.address)}</>,
+                              'copy',
+                            )
+                          }
+                          text={props.proposal?.proposer}
+                        >
+                          <Icon
+                            title="Copy"
+                            icon="duplicate"
+                            className="text-white cursor-pointer hover:text-gold ml-2"
+                            iconSize={15}
+                          />
+                        </CopyToClipboard>
+                      </div>
+                    }
+                  >
+                    <p className="text-gold text-sm tracking-normal cursor-pointer duration-300 hover:opacity-70 transition">
                       {prettyTx(props.proposal?.proposer) ||
                         '0x00000000000000000'}
-                    </>
-                  )}
-                </p>
+                    </p>
+                  </Popover2>
+                )}
               </div>
             </div>
           )}
