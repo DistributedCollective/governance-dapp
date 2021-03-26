387dfcd7c3732f462722b6751d224b1dd488f68f
Victor Creed <69458664+creed-victor@users.noreply.github.com>
2021-03-26 14:22:58 +0200

    Update src/app/containers/StakePage/components/VestingTeamTable.tsx

--- src/app/containers/StakePage/components/VestingTeamTable.tsx
+++ src/app/containers/StakePage/components/VestingTeamTable.tsx
@@ -184,7 +184,6 @@ export function VestingTeamTable() {
                   </button>
                   <button
                     type="button"
-                    disabled={locked}
                     className="text-gold tracking-normal hover:text-gold hover:no-underline hover:bg-gold hover:bg-opacity-30 mr-1 xl:mr-12 px-4 py-2 bordered transition duration-500 ease-in-out rounded-full border border-gold text-sm font-light font-montserrat"
                     onClick={handleWithdrawSubmit}
                   >
