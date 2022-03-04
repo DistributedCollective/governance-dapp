d707ef7abeb6a4da9e201baa591227e1eb41f473
soulBit <its.soulBit@gmail.com>
2022-03-04 11:28:19 +0000

    chore: fix testnet websocket url

--- src/app/containers/BlockChainProvider/classifiers.ts
+++ src/app/containers/BlockChainProvider/classifiers.ts
@@ -7,7 +7,7 @@ export const rpcNodes = {
 
 export const wssNodes = {
   30: 'wss://mainnet.sovryn.app/websocket',
-  31: 'wss://testnet.sovryn.app/ws',
+  31: 'wss://testnet.sovryn.app/websocket',
 };
 
 export const blockExplorers = {
